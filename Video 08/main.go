9034c639e461ac6d6fce306c841c526a01eb7671
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 20:54:18 +0530

    modified:   Video 08/main.go

--- Video 08/main.go
+++ Video 08/main.go
@@ -1,21 +1,25 @@
 package main
-import("fmt")
-func main(){
+
+import (
+	"fmt"
+)
+
+func main() {
 
 	var number_1 = 10
 	var number_2 = 20
 
-	var sum = number_1+number_2
-	var sub = number_1-number_2
-	var multi = number_1*number_2
-	var div = number_2/number_1
+	var sum = number_1 + number_2
+	var sub = number_1 - number_2
+	var multi = number_1 * number_2
+	var div = number_2 / number_1
 
 	fmt.Println(div)
 	fmt.Println(sub)
 	fmt.Println(sum)
 	fmt.Println(multi)
 
-	fmt.Println(number_1+number_2)
+	fmt.Println(number_1 + number_2)
 
 	fmt.Println("Assign")
 	var name = "Abhishek"
@@ -31,5 +35,5 @@ func main(){
 	fmt.Println(num_1 < num_2)
 	fmt.Println(num_1 <= num_2)
 	fmt.Println(num_1 >= num_2)
-	
+
 }
