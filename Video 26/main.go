2189fe9258cc27c10101a31153bcd2dfd84a04fa
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 21:06:58 +0530

    modified:   Video 26/main.go

--- Video 26/main.go
+++ Video 26/main.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-	slice := []int{1,2,3,4,5}
+	slice := []int{1, 2, 3, 4, 5}
 	fmt.Println(slice)
 	fmt.Println(len(slice))
 
-	array := [5]int{1,2,3,4,5}
+	array := [5]int{1, 2, 3, 4, 5}
 	slice_2 := array[0:5]
 	fmt.Println(slice_2)
 }
