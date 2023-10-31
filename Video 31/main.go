2ff954a5d0ce8fc7d5c5a0647600918845f39de6
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 21:08:46 +0530

    modified:   Video 31/main.go

--- Video 31/main.go
+++ Video 31/main.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 )
 
-func ret(x int) (z int)  {
+func ret(x int) (z int) {
 	z = x + 100
-	return 
+	return
 	// return x + 100
 }
 
-func two(age int , name string)(a int , b string)  {
+func two(age int, name string) (a int, b string) {
 	a = age
 	b = name
 	return
 }
 
-func main()  {
-	
-	fmt.Println(ret(100))
-	fmt.Println(two(2 , "Abhi"))
+func main() {
 
-	val_1 , val_2 := two(12 , "Abhishek")
+	fmt.Println(ret(100))
+	fmt.Println(two(2, "Abhi"))
+
+	val_1, val_2 := two(12, "Abhishek")
 	fmt.Println(val_1)
 	fmt.Println(val_2)
 
-	_ , val_4 := two(120 , "Abhishek kumar")
+	_, val_4 := two(120, "Abhishek kumar")
 	fmt.Println(val_4)
-
-
 
 }
