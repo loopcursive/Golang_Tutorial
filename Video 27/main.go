00e936e1deacaa2bd440c902337a77b1478557bd
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 21:07:26 +0530

    modified:   Video 27/main.go

--- Video 27/main.go
+++ Video 27/main.go
@@ -1,15 +1,15 @@
 package main
+
 import (
 	"fmt"
 )
 
 func main() {
-	age := []int{1,2,3,4,5}
+	age := []int{1, 2, 3, 4, 5}
 	fmt.Println(age)
 	fmt.Println(age[4])
 	age[4] = 100
 	fmt.Println(age)
-	age = append(age, 22,33,44)
+	age = append(age, 22, 33, 44)
 	fmt.Println(age)
 }
- 
