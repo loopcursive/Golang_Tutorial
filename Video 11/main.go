8746955b176280da06d07051e648f4336195ce6e
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 20:59:52 +0530

    modified:   Video 11/main.go

--- Video 11/main.go
+++ Video 11/main.go
@@ -1,23 +1,22 @@
 package main
-import(
-	"fmt" 
+
+import (
+	"fmt"
 	"strings"
 )
-func main(){
+
+func main() {
 	var name = "Abhi@sAAAAAAhek"
 	var name_1 = "KUMAR"
 	// fmt.Printf("%c" , name[7])
-	// fmt.Println(len(name))   
+	// fmt.Println(len(name))
 	fmt.Println(name)
 	fmt.Println(len(name))
-	fmt.Println(strings.Contains(name , "kum"))
+	fmt.Println(strings.Contains(name, "kum"))
 	fmt.Println(strings.ToLower(name_1))
 	fmt.Println(strings.ToUpper(name))
-	fmt.Println(strings.Count(name , "A"))
-	fmt.Println(strings.Index(name , "k"))
-	fmt.Println(strings.Split(name , "@"))
-
-	
-
+	fmt.Println(strings.Count(name, "A"))
+	fmt.Println(strings.Index(name, "k"))
+	fmt.Println(strings.Split(name, "@"))
 
 }
