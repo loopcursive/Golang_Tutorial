d2cc1e3be6ef884202b168fb7b71c64369747a45
Abhishek kumar <1171abhishek@gmail.com>
2023-10-31 21:04:17 +0530

    modified:   Video 21/main.go

--- Video 21/main.go
+++ Video 21/main.go
@@ -6,20 +6,20 @@ import (
 
 func main() {
 
-	 user_name := "Abhishek@123"
-	 user_password := "1234"
+	user_name := "Abhishek@123"
+	user_password := "1234"
 
-	 var input_user_name string
-	 var input_user_password string
+	var input_user_name string
+	var input_user_password string
 
-	 fmt.Println("Enter your user name")
-	 fmt.Scan(&input_user_name)
-	 fmt.Println("Enter your password")
-	 fmt.Scan(&input_user_password)
+	fmt.Println("Enter your user name")
+	fmt.Scan(&input_user_name)
+	fmt.Println("Enter your password")
+	fmt.Scan(&input_user_password)
 
-	 if user_name == input_user_name && user_password == input_user_password {
+	if user_name == input_user_name && user_password == input_user_password {
 		fmt.Println("Authentication successful")
-	 }else {
+	} else {
 		fmt.Println("Authentication fail")
-	 }
+	}
 }
